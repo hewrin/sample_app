c7077db083bce5438a72457c4edee3a91b108d2e
hewrin <hewrin@hotmail.com>
2015-03-04 22:32:41 +0800

    Finish Signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		flash[:success] = "Welcome to the Sample App!"
+  		#equivalent to redirect_to user_url(@user)
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+#strong parameters used for security.
+  	def user_params
+  		params.require(:user).permit(:name, :email, :password,:password_confirmation)
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,10 @@ Rails.application.routes.draw do
 
   get 'signup' => 'users#new'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  resources :users
+ 
 
-  # You can have the root of your site routed with "root"
+
   
 
   # Example of regular route:
