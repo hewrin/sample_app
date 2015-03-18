8e84f25d78cd3b7ea4776fe0530e65f7773de0a1
hewrin <hewrin@hotmail.com>
2015-03-18 12:29:21 +0800

    Fix typos

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,8 +7,9 @@ module SessionsHelper
     # searches for user session, if not logs in using user cookies
    if (user_id = session[:user_id])
     @current_user ||= User.find_by(id: user_id)
-  elsif (user_id = cookies.siged[:user_id])
+  elsif (user_id = cookies.signed[:user_id])
     user = User.find_by(id: user_id)
+  end
     if user && user.authenticated?(cookies[:remember_token])
       log_in user
       @current_user = user
